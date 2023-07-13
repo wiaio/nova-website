e66024e23f30efcc502de187c2211c73230c215d
naticampiglia <99177631+naticampiglia@users.noreply.github.com>
2023-07-13 15:49:38 +0200

    New hero

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,25 +1,34 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+  selector: 'app-topbar',
+  templateUrl: './app.topbar.component.html',
 })
 export class AppTopbarComponent {
+  @ViewChild('menubutton') menuButton!: ElementRef;
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
+  scrollPosition = 0;
 
-    constructor(public layoutService: LayoutService) { }
+  constructor(public layoutService: LayoutService) {}
 
-    onMenuButtonClick() {
-        this.layoutService.onMenuToggle();
-    }
+  onMenuButtonClick() {
+    this.layoutService.onMenuToggle();
+  }
 
-    onProfileButtonClick() {
-        this.layoutService.showProfileSidebar();
-    }
-    onConfigButtonClick() {
-        this.layoutService.showConfigSidebar();
-    }
-    
+  onProfileButtonClick() {
+    this.layoutService.showProfileSidebar();
+  }
+  onConfigButtonClick() {
+    this.layoutService.showConfigSidebar();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  doSomething(event: any) {
+    // console.debug("Scroll Event", document.body.scrollTop);
+    // see András Szepesházi's comment below
+    // console.log(event);
+    this.scrollPosition = window.pageYOffset;
+    console.log('Scroll Event', window.pageYOffset);
+  }
 }
